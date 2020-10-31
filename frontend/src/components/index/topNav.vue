77dd498e488dd42a6c3c17dc37282dcd63a8ba4d
hasanhoztunc <hasanhuseyin9e@gmail.com>
2020-10-31 21:51:52 +0300

    frontend initialized

---
+++ frontend/src/components/index/topNav.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="topNav">
+        <div class="brandName">
+            {{ $t('BRAND_NAME') }}
+        </div>
+        <customInput />
+    </div>
+</template>
+
+<script>
+import customInput from '../utils/input'
+
+export default {
+    name: 'TopNav',
+    components: {
+        customInput
+    }
+}
+</script>
+
+<style scoped>
+    .topNav {
+        width: 80%;
+        display: flex;
+        flex: 1;
+        flex-direction: row;
+        justify-content: flex-start;
+        font-size: 26px;
+        padding: 20px;
+        font-weight: 800;
+    }
+    .brandName {
+        margin-left: 20px;
+        color: rgb(255,96,0);
+    }
+</style>
